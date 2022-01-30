29590e9fcaf3995efdc3134c725867214d922417
Nick Cooper <nccooper00@gmail.com>
2022-01-29 17:25:31 -0800

    struggling to figure out how to make it so when you delete a note and make a new one the old ones that were deleted dont reappear. rearranging the order of the get, delete, and post requests to test

--- server.js
+++ server.js
@@ -2,47 +2,44 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const notes = require('./db/db.json');
-
+const uuid = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
+
 
 app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-    res.json(notes);
+    res.sendFile(path.join(__dirname, './db/db.json'));
 });
 
 app.post('/api/notes', (req, res) => {
     console.info(`${req.method} request received to add a note`);
   
-    const { title, text } = req.body;
-  
+    const {title, text} = req.body;
+
     if (title && text) {
       const newNote = {
         title,
-        text
+        text,
+        id: uuid.v4()
     };
+
+    notes.push(newNote);
+
+    const noteString = JSON.stringify(notes, null, 4)
 
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
           console.error(err);
         } else {
-          const parsedNotes = JSON.parse(data);
-  
-          parsedNotes.push(newNote);
-  
           fs.writeFile(
             './db/db.json',
-            JSON.stringify(parsedNotes, null, 4),
+            noteString,
             (writeErr) =>
               writeErr
                 ? console.error(writeErr)
@@ -61,6 +58,21 @@ app.post('/api/notes', (req, res) => {
     } else {
       res.status(500).json('Error in posting note');
     }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const NOTES = JSON.parse(fs.readFileSync('./db/db.json'));
+  const deleteNote = NOTES.filter((removeNote) => removeNote.id !== req.params.id);
+  fs.writeFileSync('./db/db.json', JSON.stringify(deleteNote));
+  res.json(deleteNote);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 app.listen(PORT, () => 
